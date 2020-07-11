84e3390063d99c82fe03472261c527af3489a435
Juliano Alves <contato.julianoalvess@gmail.com>
2020-07-11 14:48:20 -0300

    style: add style header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 
+import { ContainerHeader } from './styles';
+
 const Header = () => (
     <>
-        <h1>Header</h1>
+        <ContainerHeader>
+            <h1 className="header-title">
+                React<span className="header-span">JS</span>
+            </h1>
+        </ContainerHeader>
     </>
 );
 
---
+++ src/components/Header/styles.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const ContainerHeader = styled.header`
+    background-color: #333333;
+    padding: 1rem 0;
+    height: 80px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    .header-title {
+        color: #fff;
+
+        .header-span {
+            color: '#ff5500';
+        }
+    }
+`;
